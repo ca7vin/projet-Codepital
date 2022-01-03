c8e9606cb2e68d32f655ee9b22029b812a2510ef
ca7vin <ca7vin@gmail.com>
2022-01-03 11:15:47 +0100

    Methodes pharmacie

--- public/js/class.js
+++ public/js/class.js
@@ -1,5 +1,5 @@
 export class Patients {
-    constructor(nom, maladie, argent, poche, etatSante, traitement){
+    constructor(nom, maladie, argent, poche, etatSante, traitement) {
         this.nom = nom;
         this.maladie = maladie;
         this.argent = argent;
@@ -7,7 +7,7 @@ export class Patients {
         this.etatSante = etatSante;
         this.traitement = traitement;
     }
-    goTo(depart,arrivee){
+    goTo(depart, arrivee) {
         switch (depart) {
             case docteur:
                 docteur.cabinet.splice(docteur.cabinet.indexOf(this.nom), 1);
@@ -20,11 +20,105 @@ export class Patients {
         }
         console.log(`${this.nom} se déplace de ${depart} à ${arrivee}`)
     }
-    takeCare(){
+    takeCare() {
 
     }
-    payer(){
+    payer(target) {
+        switch (target) {
+            case docteur:
+                this.argent -= 50;
+                docteur.argent += 50;
+                console.log(`${this.nom} paye sa consultation au ${docteur.nom}`)
+                break;
+            case pharmacie:
+                switch (this.traitement) {
+                    case "ctrl+maj+f":
+                        console.log(`${this.nom} est atteint de ${this.maladie} et a demande ${this.traitement}`)
+                        if (this.argent >= 60) {
+                            console.log(`${this.nom} a assez d'argent pour acheter son traitement et le paye`)
+                            this.argent -= 60
+                            pharmacie.argent += 60
+                            this.poche.push("ctrl+maj+f")
+                            console.log(`${this.nom} reçoit ${this.traitement}`)
+                        } else {
+                            console.log(`${this.nom} n'a pas assez d'argent pour payer son traitement`)
+                            cimetiere.personnes.push(this)
+                            pharmacie.personnes.splice(pharmacie.personnes.indexOf(this), 1)
+                            console.log(`${this.nom} meurt de ${this.maladie} et est transporté au ${cimetiere.nom}`)
+                            this.etatSante = "mort"
+                        }
+                        break;
 
+                    case "saveOnFocusChange":
+                        console.log(`${this.nom} est atteint de ${this.maladie} et a demande ${this.traitement}`)
+                        if (this.argent >= 100) {
+                            console.log(`${this.nom} a assez d'argent pour acheter son traitement et le paye`)
+                            this.argent -= 100
+                            pharmacie.argent += 100
+                            this.poche.push("saveOnFocusChange")
+                            console.log(`${this.nom} reçoit ${this.traitement}`)
+                        } else {
+                            console.log(`${this.nom} n'a pas assez d'argent pour payer son traitement`)
+                            cimetiere.personnes.push(this)
+                            pharmacie.personnes.splice(pharmacie.personnes.indexOf(this), 1)
+                            console.log(`${this.nom} meurt de ${this.maladie} et est transporté au ${cimetiere.nom}`)
+                            this.etatSante = "mort"
+                        }
+                        break;
+
+                    case "checkLinkRelation":
+                        console.log(`${this.nom} est atteint de ${this.maladie} et a demande un traitement ${this.traitement}`)
+                        if (this.argent >= 35) {
+                            console.log(`${this.nom} a assez d'argent pour acheter son traitement et le paye`)
+                            this.argent -= 35
+                            pharmacie.argent += 35
+                            this.poche.push("checkLinkRelation")
+                            console.log(`${this.nom} reçoit ${this.traitement}`)
+                        } else {
+                            console.log(`${this.nom} n'a pas assez d'argent pour payer son traitement`)
+                            cimetiere.personnes.push(this)
+                            pharmacie.personnes.splice(pharmacie.personnes.indexOf(this), 1)
+                            console.log(`${this.nom} meurt de ${this.maladie} et est transporté au ${cimetiere.nom}`)
+                            this.etatSante = "mort"
+                        }
+                        break;
+
+                    case "Ventoline":
+                        console.log(`${this.nom} est atteint de ${this.maladie} et a demande ${this.traitement}`)
+                        if (this.argent >= 40) {
+                            console.log(`${this.nom} a assez d'argent pour acheter son traitement et le paye`)
+                            this.argent -= 40
+                            pharmacie.argent += 40
+                            this.poche.push("Ventoline")
+                            console.log(`${this.nom} reçoit ${this.traitement}`)
+                        } else {
+                            console.log(`${this.nom} n'a pas assez d'argent pour payer son traitement`)
+                            cimetiere.personnes.push(this)
+                            pharmacie.personnes.splice(pharmacie.personnes.indexOf(this), 1)
+                            console.log(`${this.nom} meurt de ${this.maladie} et est transporté au ${cimetiere.nom}`)
+                            this.etatSante = "mort"
+                        }
+                        break;
+
+                    case "f12+doc":
+                        console.log(`${this.nom} est atteint de ${this.maladie} et a demande ${this.traitement}`)
+                        if (this.argent >= 20) {
+                            console.log(`${this.nom} a assez d'argent pour acheter son traitement et le paye`)
+                            this.argent -= 20
+                            pharmacie.argent += 20
+                            this.poche.push("f12+doc")
+                            console.log(`${this.nom} reçoit ${this.traitement}`)
+                        } else {
+                            console.log(`${this.nom} n'a pas assez d'argent pour payer son traitement`)
+                            cimetiere.personnes.push(this)
+                            pharmacie.personnes.splice(pharmacie.personnes.indexOf(this), 1)
+                            console.log(`${this.nom} meurt de ${this.maladie} et est transporté au ${cimetiere.nom}`)
+                            this.etatSante = "mort"
+                        }
+                        break;
+                }
+                break;
+        }
     }
 }
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,3 @@
-// # Bienvenu à Codepital:
-// >Dans cet exercice nous aurons des malades qui iront ce faire débuger chez un doctor. Le doctor les diagnostiquera et leur préscrira un remède. Par la suite les malades irons à la pharmacie afin d'acheter leur remède puis le prendrons et seront guérri.
-
-// ## Description des patients
-// >les malades ont un nom, une maladie, de l'argent, une poche, un état de santé,ils savent aller à un endroit, prendre un médicamment et payer. Au début, les patients sont dans un salle d'attente. 
-
-// |nom|maladie|argent|poche|etatSante|traitement|
-// goTo|takeCare|paye| METHODES
-// |---|---|---|---|---|---|---|---|---|
 
 // ## Description du doctor
 // >Le doctor lui reçoit les patients dans son cabinet. Tout d'abord il les diagnostiques puis se fait payé avant de préscrire un traitement. Attention le doctor fait à chaque fois sortir le patient de son cabinet avant de prendre le suivant. Dans son cabinet il y a son chat de race sphynx pour garder un environemment stérile. Son chat miaule toutes les deux secondes dans la console(bonus). La consultation coûte 50€. Les patients son dans un état de traitement avant de sortir du cabinet.
@@ -51,73 +42,75 @@ let semicolon = new Patients("Semicolon", "syntaxError", 60, [], "malade", "")
 
 /* MAISON */
 let maison = {
-    nom : "maison",
+    nom: "maison",
     personnes: [],
 }
 
 /* CIMETIERE */
 let cimetiere = {
-    nom : "cimetiere",
+    nom: "cimetiere",
     personnes: [],
 }
 
 /* PHARMACIE */
 let pharmacie = {
-    nom : "pharmacie",
-    personnes : [],
-    traitements : ["Ventoline", "f12+doc", "checkLinkRelation", "saveOnFocusChange", "ctrl+maj+f"],
-    argent : 150
+    nom: "pharmacie",
+    personnes: [],
+    traitements: ["Ventoline", "f12+doc", "checkLinkRelation", "saveOnFocusChange", "ctrl+maj+f"],
+    argent: 150
 }
 
 /* DOCTEUR */
-// ### Grille des diagnostiques
-// |maladie|traitement|
-// |---|---|
-// |mal indenté|`ctrl+maj+f`|
-// |unsave|`saveOnFocusChange`|
-// |404|`CheckLinkRelation`|
-// |azmatique|`Ventoline`|
-// |syntaxError|`f12+doc`|
 let docteur = {
-    nom : "docteur Paul",
-    argent : 350,
-    cabinet : [],
-    salleAttente : [],
-    diagnostique(diaTarget){
+    nom: "docteur Paul",
+    argent: 350,
+    cabinet: [],
+    salleAttente: [marcus, optimus, sangoku, darthVader, semicolon],
+    diagnostique(diaTarget) {
         console.log(`${this.nom} examine ${diaTarget.nom} pour lui donner un diagnostic`)
         switch (diaTarget.maladie) {
             case "mal indenté":
-                console.log(`${diaTarget.nom} est mal indenté et a besoin de ctrl+maj+f comme traitement`)
+                diaTarget.traitement = "ctrl+maj+f"
+                console.log(`${diaTarget.nom} est mal indenté et a besoin de ${diaTarget.traitement}`)
                 break;
             case "unsave":
-                console.log(`${diaTarget.nom} est unsave et a besoin de saveOnFocusChange comme traitement`)
+                diaTarget.traitement = "saveOnFocusChange"
+                console.log(`${diaTarget.nom} est unsave et a besoin de ${diaTarget.traitement}`)
                 break;
             case "404":
-                console.log(`${diaTarget.nom} est en 404 et a besoin de checkLinkRelation comme traitement`)
+                diaTarget.traitement = "checkLinkRelation"
+                console.log(`${diaTarget.nom} est en 404 et a besoin de ${diaTarget.traitement}`)
                 break;
             case "azmatique":
-                console.log(`${diaTarget.nom} est azmatique et a besoin de Ventoline comme traitement`)
+                diaTarget.traitement = "Ventoline"
+                console.log(`${diaTarget.nom} est azmatique et a besoin de ${diaTarget.traitement}`)
                 break;
             case "syntaxError":
-                console.log(`${diaTarget.nom} est atteint de syntaxError et a besoin de f12+doc comme traitement`)
+                diaTarget.traitement = "f12+doc"
+                console.log(`${diaTarget.nom} est atteint de syntaxError et a besoin de ${diaTarget.traitement}`)
         }
         console.log(`${this.nom} attend son paiement avant de vous donner votre prescription`)
         if (diaTarget.argent >= 50) {
-            diaTarget.argent -= 50;
-            this.argent += 50;
-            console.log(`${diaTarget.nom} paye sa consultation à ${this.nom} et reçoit sa prescription`)
+            diaTarget.payer(docteur)
+            diaTarget.etatSante = "traitement"
         } else {
             console.log(`${diaTarget.nom} n'a pas assez d'argent et ne peut pas payer sa consultation`)
+            diaTarget.etatSante = "mort"
             cimetiere.personnes.push(diaTarget)
+            docteur.cabinet.splice(docteur.cabinet.indexOf(diaTarget), 1)
             console.log(`${diaTarget.nom} meurt de ${diaTarget.maladie} et est transporté au ${cimetiere.nom}`)
         }
     },
-    patientIn(target){
-        console.log(`${this.nom} appelle ${target.nom}`)
-        docteur.cabinet.push(target)
-        docteur.salleAttente.splice(docteur.salleAttente.indexOf(target), 1)
+    patientIn(target) {
+        if (docteur.cabinet.length > 0) {
+            console.log(`${this.nom} appelle ${target.nom}`)
+            docteur.cabinet.push(target)
+            docteur.salleAttente.splice(docteur.salleAttente.indexOf(target), 1)
+        } else {
+            console.log(`Il reste encore ${target.nom} dans le cabinet !`)
+        }
     },
-    patientOut(target){
+    patientOut(target) {
         docteur.salleAttente.push(target);
         docteur.cabinet.splice(docteur.cabinet.indexOf(target), 1)
         console.log(`${this.nom} a finit sa consultation et fait sortir ${target.nom}`)
