3a00dfefedbe5341bf3220c55946cd3a5ee44f95
ca7vin <ca7vin@gmail.com>
2022-01-03 09:37:23 +0100

    Class Patients

---
+++ public/js/class.js
@@ -0,0 +1,23 @@
+export class Patients {
+    constructor(nom, maladie, argent, poche, etatSante, traitement){
+        this.nom = nom;
+        this.maladie = maladie;
+        this.argent = argent;
+        this.poche = poche;
+        this.etatSante = etatSante;
+        this.traitement = traitement;
+    }
+    goTo(){
+
+    }
+    takeCare(){
+
+    }
+    payer(){
+
+    }
+}
+
+
+
+
